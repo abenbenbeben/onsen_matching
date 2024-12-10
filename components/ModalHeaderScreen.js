5a2630d5a6082c71816d2401017745bba86e868e
abebe <rebuildaby@gmail.com>
2024-12-10 22:40:45 +0900

    モーダルボタン追加

--- components/ModalHeaderScreen.js
+++ components/ModalHeaderScreen.js
@@ -1,15 +1,30 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-import { StyleSheet, Animated, Text } from "react-native";
+import { StyleSheet, Animated, Text, Pressable } from "react-native";
 import { Color, FontSize } from "../GlobalStyles";
+import { IconButton } from "react-native-paper";
 
-const HeaderScreen = ({
+const ModalHeaderScreen = ({
   headerText = "スーパー銭湯マッチング",
   headerHeight = 90,
+  onPress_cancelButton = () => {},
+  deleteTextLabel = "削除",
 }) => {
   return (
     <Animated.View style={[styles.wrapper, { height: headerHeight }]}>
+      <IconButton
+        icon={"window-close"}
+        iconColor={Color.labelColorDarkPrimary}
+        selected="true"
+        size={30}
+        style={[styles.closeButton]}
+      />
       <Text style={[styles.headerText]}>{headerText}</Text>
+      <Pressable
+        style={styles.deleteButton}
+        onPress={() => onPress_cancelButton()}
+      >
+        <Text style={styles.deleteButtonText}>{deleteTextLabel}</Text>
+      </Pressable>
     </Animated.View>
   );
 };
@@ -18,14 +33,33 @@ const styles = StyleSheet.create({
   wrapper: {
     backgroundColor: Color.colorMain,
     alignItems: "center",
-    justifyContent: "flex-end",
+    flexDirection: "row",
   },
   headerText: {
+    width: "100%",
+    textAlign: "center",
     fontSize: FontSize.bodySub,
     fontWeight: "500",
     color: Color.labelColorDarkPrimary,
-    marginBottom: 14, // 下から10の位置に配置
+    position: "absolute",
+    bottom: 16, // 下から5の位置に配置
+  },
+  closeButton: {
+    position: "absolute",
+    bottom: 3, // 下から5の位置に配置
+    left: 10,
+    margin: 0,
+  },
+  deleteButton: {
+    position: "absolute",
+    right: 16,
+    bottom: 16, // 下から5の位置に配置
+  },
+  deleteButtonText: {
+    fontSize: FontSize.bodySub,
+    fontWeight: "500",
+    color: Color.labelColorDarkPrimary,
   },
 });
 
-export default HeaderScreen;
+export default ModalHeaderScreen;
